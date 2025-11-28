b7afaefc62ea9fed262359367e42b69c667f17f3
Srirup Banerjee <srib@tdcnet.dk>
2025-11-28 12:37:53 +0100

    refer image correctly

--- main.tf
+++ main.tf
@@ -133,7 +133,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   os_disk {
     storage_account_type = "Standard_LRS"
     caching              = "ReadWrite"
-	image_uri = data.azurerm_image.packer_image.id
+	managed_disk_id = data.azurerm_image.packer_image.id
   }
 }
 
